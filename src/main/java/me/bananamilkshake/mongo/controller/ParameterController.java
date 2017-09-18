483d3c53fa795719fbd2063aca84cb8e3afb595b
bananamilkshake <me@bananamilkshake.me>
2017-09-18 23:37:46 +0300

    Better name for controller.

--- src/main/java/me/bananamilkshake/mongo/controller/ParameterController.java
+++ src/main/java/me/bananamilkshake/mongo/controller/ParameterController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/parameter/{type}")
 @AllArgsConstructor
-public class Controller {
+public class ParameterController {
 
 	private final ParameterResponseAssembler parameterResponseAssembler;
 
