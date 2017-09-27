fd1d1df10fb968af8d5d3d2be323337793824123
bananamilkshake <me@bananamilkshake.me>
2017-09-27 23:32:54 +0300

    Fix mistype.

--- src/main/java/me/bananamilkshake/mongo/domain/Parameter.java
+++ src/main/java/me/bananamilkshake/mongo/domain/Parameter.java
@@ -1,7 +1,7 @@
 package me.bananamilkshake.mongo.domain;
 
 /**
- * This interface is created to trace any changes in nesessary parameter
+ * This interface is created to trace any changes in necessary parameter
  * fields. Query and Validation classes should be implemented from that.
  */
 public interface Parameter {
