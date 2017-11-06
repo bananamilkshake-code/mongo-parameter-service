e66ca272aa9ed406d7409b602a369424eebed5a1
bananamilkshake <me@bananamilkshake.me>
2017-11-06 13:11:44 +0300

    Extract field names to separated class.

---
+++ src/main/java/me/bananamilkshake/mongo/domain/Fields.java
@@ -0,0 +1,8 @@
+package me.bananamilkshake.mongo.domain;
+
+public class Fields {
+
+	public static final String USER = "user";
+	public static final String VALID_FROM = "validFrom";
+	public static final String VALUES = "values";
+}
--- src/main/java/me/bananamilkshake/mongo/service/values/ValuesPreparationService.java
+++ src/main/java/me/bananamilkshake/mongo/service/values/ValuesPreparationService.java
@@ -3,6 +3,7 @@ package me.bananamilkshake.mongo.service.values;
 import com.google.common.base.Joiner;
 import com.mongodb.BasicDBObject;
 import com.mongodb.util.JSON;
+import me.bananamilkshake.mongo.domain.Fields;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -11,8 +12,6 @@ import java.util.stream.Collectors;
 
 @Component
 public class ValuesPreparationService {
-
-	private static final String FIELD_VALUES = "values";
 
 	public Map<String, Object> prepare(String description) {
 		return readValues(description).entrySet()
@@ -26,7 +25,7 @@ public class ValuesPreparationService {
 	}
 
 	private Map.Entry<String, Object> convert(Map.Entry<String, Object> entry) {
-		String newKey = Joiner.on('.').join(FIELD_VALUES, entry.getKey());
+		String newKey = Joiner.on('.').join(Fields.VALUES, entry.getKey());
 		return new HashMap.SimpleEntry<>(newKey, entry.getValue());
 	}
 }
