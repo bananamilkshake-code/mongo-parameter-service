e0aee26dc2bfc9f1a5f9094cab70f658397b4249
bananamilkshake <me@bananamilkshake.me>
2017-11-27 01:05:55 +0300

    Integration test should be able tto use MockMvc.

--- parameter-service/src/test/java/me/bananamilkshake/mongo/IntegrationTest.java
+++ parameter-service/src/test/java/me/bananamilkshake/mongo/IntegrationTest.java
@@ -3,11 +3,13 @@ package me.bananamilkshake.mongo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 @WebAppConfiguration
 public class IntegrationTest {
 
