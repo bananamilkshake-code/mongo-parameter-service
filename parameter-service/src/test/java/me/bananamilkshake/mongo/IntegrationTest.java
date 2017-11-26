7f1399c35cac71a33db13de22f7c2b2168903f74
bananamilkshake <me@bananamilkshake.me>
2017-11-27 01:06:14 +0300

    No integration tests noe.

--- parameter-service/src/test/java/me/bananamilkshake/mongo/IntegrationTest.java
+++ parameter-service/src/test/java/me/bananamilkshake/mongo/IntegrationTest.java
@@ -1,5 +1,6 @@
 package me.bananamilkshake.mongo;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @WebAppConfiguration
